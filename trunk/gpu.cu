ee4401e7d7cee7aa7819f4b32e334bbaddcc521f
dmitryfizteh <dmitryfizteh@50e58543-5401-0a7e-3a26-93843113db51>
2011-10-21 20:27:11 +0000

    Функции и определения CUDA возвращены в gpu.cu (так как куда не понимает extern функций и константных переменных). Поправлен баг в расчетах скоростей на gpu. Поправлен вывод графиков.

--- trunk/gpu.cu
+++ trunk/gpu.cu
@@ -1,6 +1,8 @@
 #include "defines.h"
-#include "gpu.h"
+//#include "gpu.h"
+#include <cuda.h>
 
+__constant__ consts gpu_def [1];
 
 // Проверка ошибок GPU
 void checkErrors(char *label) 
@@ -53,6 +55,88 @@ __device__ int device_is_active_point(int i, int localNx, int rank, int size)
 	else
 		return 1;
 }
+
+//========================================================================================================================
+// Очень желательно перенести в отдельные файлы проектов. Но пока невозможно.
+
+// Расчет плотностей, давления NAPL P2 и Xi в каждой точке сетки (независимо от остальных точек)
+__global__ void assign_ro_Pn_Xi_kernel(ptr_Arrays DevArraysPtr, int localNx, int rank, int size) 
+{
+	int blockIdxz=blockIdx.y / BlockNY;
+	int blockIdxy=blockIdx.y % BlockNY;
+	int i=threadIdx.x+blockIdx.x*blockDim.x;
+	int k=threadIdx.z+blockIdxz*blockDim.z;
+	int j=threadIdx.y+blockIdxy*blockDim.y;
+
+	if ((i<localNx) && (j<((*gpu_def).Ny)) && (k<((*gpu_def).Nz)) && (device_is_active_point(i, localNx, rank, size)==1))
+	{
+		int media = DevArraysPtr.media[i+j*localNx+k*localNx*((*gpu_def).Ny)];
+		double S_n = DevArraysPtr.S_n[i+j*localNx+k*localNx*((*gpu_def).Ny)];
+		double P_w = DevArraysPtr.P_w[i+j*localNx+k*localNx*((*gpu_def).Ny)];
+
+		double S_e = (1 - S_n - (*gpu_def).S_wr[media]) / (1 - (*gpu_def).S_wr[media]);
+		double k_w = pow(S_e, (2. + 3. * (*gpu_def).lambda[media]) / (*gpu_def).lambda[media]);
+		double k_n = (1. - S_e) * (1. - S_e) * (1 - pow(S_e, (2 + (*gpu_def).lambda[media]) / ((*gpu_def).lambda[media])));
+		double P_k = (*gpu_def).P_d[media] * pow((1 - S_n - (*gpu_def).S_wr[media]) / (1 - (*gpu_def).S_wr[media]), -1 / (*gpu_def).lambda[media]);
+
+		DevArraysPtr.P_n[i+j*localNx+k*localNx*((*gpu_def).Ny)] = P_w + P_k;
+		DevArraysPtr.Xi_w[i+j*localNx+k*localNx*((*gpu_def).Ny)] = -1 * (*gpu_def).K[media] * k_w / (*gpu_def).mu_w;
+		DevArraysPtr.Xi_n[i+j*localNx+k*localNx*((*gpu_def).Ny)] = -1 * (*gpu_def).K[media] * k_n / (*gpu_def).mu_n;
+		DevArraysPtr.ro_w[i+j*localNx+k*localNx*((*gpu_def).Ny)] = (*gpu_def).ro0_w * (1 + ((*gpu_def).beta_w) * (P_w - (*gpu_def).P_atm));
+		DevArraysPtr.ro_n[i+j*localNx+k*localNx*((*gpu_def).Ny)] = (*gpu_def).ro0_n * (1 + ((*gpu_def).beta_n) * (P_w + P_k - (*gpu_def).P_atm));
+	}
+}
+
+// Расчет плотностей, давления NAPL P2 и Xi во всех точках сетки
+void ro_P_Xi_calculation(ptr_Arrays HostArraysPtr, ptr_Arrays DevArraysPtr, consts def, int localNx, int rank, int size, int blocksX, int blocksY, int blocksZ)
+{
+	assign_ro_Pn_Xi_kernel<<<dim3(blocksX,blocksY*blocksZ), dim3(BlockNX,BlockNY,BlockNZ)>>>(DevArraysPtr,localNx,rank,size); 
+	checkErrors("assign Pn, Xi, ro");
+}
+
+// Метод Ньютона для каждой точки сетки (независимо от остальных точек)
+__global__ void Newton_method_kernel(ptr_Arrays DevArraysPtr, int localNx) 
+{
+	int blockIdxz=blockIdx.y / BlockNY;
+	int blockIdxy=blockIdx.y % BlockNY;
+	int i=threadIdx.x+blockIdx.x*blockDim.x;
+	int k=threadIdx.z+blockIdxz*blockDim.z;
+	int j=threadIdx.y+blockIdxy*blockDim.y;
+
+	if ((i<localNx-1) && (j<(*gpu_def).Ny-1) && (k<(*gpu_def).Nz) && (i!=0) && (j!=0) && (((k!=0) && (k!=(*gpu_def).Nz-1)) || ((*gpu_def).Nz<2)))
+	{
+		int media = DevArraysPtr.media[i+j*localNx+k*localNx*((*gpu_def).Ny)];
+		double S_e, P_k, AAA, F1, F2, PkS, F1P, F2P, F1S, F2S, det;
+		double S_n=DevArraysPtr.S_n[i+j*localNx+k*localNx*((*gpu_def).Ny)];
+		double P_w=DevArraysPtr.P_w[i+j*localNx+k*localNx*((*gpu_def).Ny)];
+
+		S_e = (1 - S_n - (*gpu_def).S_wr[media]) / (1 - (*gpu_def).S_wr[media]);
+		P_k = (*gpu_def).P_d[media] * pow(S_e, -1 / (*gpu_def).lambda[media]);
+		AAA = pow(S_e, ((-1 / (*gpu_def).lambda[media]) - 1));
+		F1 = (*gpu_def).ro0_w * (1 + ((*gpu_def).beta_w) * (P_w - (*gpu_def).P_atm)) * (1 - S_n) - DevArraysPtr.roS_w[i+j*localNx+k*localNx*((*gpu_def).Ny)];
+		F2 = (*gpu_def).ro0_n * (1 + ((*gpu_def).beta_n) * (P_w + P_k - (*gpu_def).P_atm)) * S_n - DevArraysPtr.roS_n[i+j*localNx+k*localNx*((*gpu_def).Ny)];
+
+		PkS = AAA * (*gpu_def).P_d[media] / ((*gpu_def).lambda[media] * (1 - (*gpu_def).S_wr[media]));
+		F1P = (*gpu_def).ro0_w * ((*gpu_def).beta_w) * (1 - S_n);
+		F2P = (*gpu_def).ro0_n * ((*gpu_def).beta_n) * S_n;
+		F1S = (-1) * (*gpu_def).ro0_w * (1 + ((*gpu_def).beta_w) * (P_w - (*gpu_def).P_atm));
+		F2S = (*gpu_def).ro0_n * (1 + ((*gpu_def).beta_n) * (P_w + P_k - (*gpu_def).P_atm + (S_n * PkS)));
+
+		det = F1P * F2S - F1S * F2P;
+
+		DevArraysPtr.P_w[i+j*localNx+k*localNx*((*gpu_def).Ny)] = P_w - (1 / det) * (F2S * F1 - F1S * F2);
+		DevArraysPtr.S_n[i+j*localNx+k*localNx*((*gpu_def).Ny)] = S_n - (1 / det) * (F1P * F2 - F2P * F1);
+	}
+}
+
+// Расчет давления воды P1 и насыщенности NAPL S2 во всех точках сетки
+void P_S_calculation(ptr_Arrays HostArraysPtr, ptr_Arrays DevArraysPtr, consts def, int localNx, int rank, int size, int blocksX, int blocksY, int blocksZ)
+{
+	Newton_method_kernel<<<dim3(blocksX,blocksY*blocksZ), dim3(BlockNX,BlockNY,BlockNZ)>>>(DevArraysPtr,localNx); 
+	checkErrors("assign Pw and Sn");
+}
+
+//========================================================================================================================
 
 // Расчет скорости в каждой точке сетки
 __global__ void assign_u_kernel(ptr_Arrays DevArraysPtr, int localNx, int rank, int size) 
@@ -131,7 +215,7 @@ __global__ void assign_u_kernel(ptr_Arrays DevArraysPtr, int localNx, int rank, 
 				DevArraysPtr.uz_w[i+j*localNx+k*localNx*((*gpu_def).Ny)] = Xi_w * (P_w - DevArraysPtr.P_w[i+j*localNx+(k-1)*localNx*((*gpu_def).Ny)]) / ((*gpu_def).hz);
 				DevArraysPtr.uz_n[i+j*localNx+k*localNx*((*gpu_def).Ny)] = Xi_n * (P_n - DevArraysPtr.P_n[i+j*localNx+(k-1)*localNx*((*gpu_def).Ny)]) / ((*gpu_def).hz);
 			}
-			if ((k != 0) && (i != ((*gpu_def).Nz) - 1))
+			if ((k != 0) && (k != ((*gpu_def).Nz) - 1))
 			{
 				DevArraysPtr.uz_w[i+j*localNx+k*localNx*((*gpu_def).Ny)] = Xi_w * ((DevArraysPtr.P_w[i+j*localNx+(k+1)*localNx*((*gpu_def).Ny)] - DevArraysPtr.P_w[i+j*localNx+(k-1)*localNx*((*gpu_def).Ny)]) / (2 * ((*gpu_def).hz)));
 				DevArraysPtr.uz_n[i+j*localNx+k*localNx*((*gpu_def).Ny)] = Xi_n * ((DevArraysPtr.P_n[i+j*localNx+(k+1)*localNx*((*gpu_def).Ny)] - DevArraysPtr.P_n[i+j*localNx+(k-1)*localNx*((*gpu_def).Ny)]) / (2 * ((*gpu_def).hz)));
@@ -508,7 +592,10 @@ void device_initialization(int rank, int* blocksX, int* blocksY, int* blocksZ, i
 	if ((def.Nz)%BlockNZ!=0)
 		(*blocksZ)++;
 
-	cudaMemcpyToSymbol ( gpu_def, &def, sizeof ( consts ), 0, cudaMemcpyHostToDevice );
+	consts* deff=new consts[1];
+	deff[0]=def;
+
+	cudaMemcpyToSymbol ( gpu_def, deff, sizeof ( consts ));//, 0, cudaMemcpyHostToDevice );
 	checkErrors("constant memory copy");
 
 	int deviceCount;
