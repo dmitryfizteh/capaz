8db84b427aec0fb28a4ab2f83c360fa6242c1366
dmitryfizteh <dmitryfizteh@50e58543-5401-0a7e-3a26-93843113db51>
2011-10-30 19:45:01 +0000

    Исправлена ошибка компиляции без параметра MY_TEST.

--- gpu.h
+++ gpu.h
@@ -1,9 +1,7 @@
 #include "defines.h"
 #include <cuda.h>
 
-#ifdef MY_TEST
 void gpu_test_nan(double* HostArrayPtr, double* DevArrayPtr, int rank, int size, int localNx, consts def, char* file, int line);
 void checkErrors(char *label);
-#endif
 
 
