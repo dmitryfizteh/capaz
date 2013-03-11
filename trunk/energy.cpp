4122bcb030162c57cbe6a7f0919b1bc24677883a
nastenka.aesc <nastenka.aesc@50e58543-5401-0a7e-3a26-93843113db51>
2013-03-11 22:34:39 +0000

    UNSTABLE.
    Упрощены аргументы функций вычисления теплоемкостей и теплопроводностей.
    Добавлены функции вычисления энтальпий (как интеграла по методу Симпсона теплоемкости по температуре).

--- trunk/energy.cpp
+++ trunk/energy.cpp
@@ -21,43 +21,43 @@ double C_g = 0.119;
 double C_r = 0.75;
 
 // Коэффициенты удельных теплоемкостей при постоянном давлении  для water, napl, gas and rock в Вт/(м*К)
-double с_w (ptr_Arrays HostArraysPtr, ptr_Arrays DevArraysPtr, int local, consts def)
+double с_w (double T, consts def)
 {
-	return c0_w - C_w * (HostArraysPtr.T[local] - T_0) + C_w2 * (HostArraysPtr.T[local] - T_0) * (HostArraysPtr.T[local] - T_0);
+	return c0_w - C_w * (T - T_0) + C_w2 * (T - T_0) * (T - T_0);
 }
 
-double с_n (ptr_Arrays HostArraysPtr, ptr_Arrays DevArraysPtr, int local, consts def)
+double с_n (double T, consts def)
 {
-	return c0_n + C_n * (HostArraysPtr.T[local] - T_0);
+	return c0_n + C_n * (T - T_0);
 }
 
-double с_g (ptr_Arrays HostArraysPtr, ptr_Arrays DevArraysPtr, int local, consts def)
+double с_g (double T, consts def)
 {
-	return c0_g + C_g * (HostArraysPtr.T[local] - T_0);
+	return c0_g + C_g * (T - T_0);
 }
 
-double с_r (ptr_Arrays HostArraysPtr, ptr_Arrays DevArraysPtr, int local, consts def)
+double с_r (double T, consts def)
 {
-	return c0_r + C_r * (HostArraysPtr.T[local] - T_0);
+	return c0_r + C_r * (T - T_0);
 }
 
 // Коэффициенты теплопроводности для water, napl, gas and rock
-double lambda_w (ptr_Arrays HostArraysPtr, ptr_Arrays DevArraysPtr, int local, consts def)
+double lambda_w (double T, consts def)
 {
-	return lambda0_w * (1 - lambdaA_w * (HostArraysPtr.T[local] - T_0));
+	return lambda0_w * (1 - lambdaA_w * (T - T_0));
 }
 
-double lambda_n (ptr_Arrays HostArraysPtr, ptr_Arrays DevArraysPtr, int local, consts def)
+double lambda_n (double T, consts def)
 {
-	return lambda0_n * (1 - lambdaA_n * (HostArraysPtr.T[local] - T_0));
+	return lambda0_n * (1 - lambdaA_n * (T - T_0));
 }
 
-double lambda_g (ptr_Arrays HostArraysPtr, ptr_Arrays DevArraysPtr, int local, consts def)
+double lambda_g (double T, consts def)
 {
-	return lambda0_g * pow((HostArraysPtr.T[local] / T_0), lambdaA_g);
+	return lambda0_g * pow((T / T_0), lambdaA_g);
 }
 
-double lambda_r (ptr_Arrays HostArraysPtr, ptr_Arrays DevArraysPtr, int local, consts def)
+double lambda_r (double T, consts def)
 {
 	return lambda0_r;
 }
@@ -65,13 +65,110 @@ double lambda_r (ptr_Arrays HostArraysPtr, ptr_Arrays DevArraysPtr, int local, c
 // Эффективный коэффициент теплопроводности в точке (будет использоваться при расчете теплового потока)
 double assign_lambda_eff (ptr_Arrays HostArraysPtr, ptr_Arrays DevArraysPtr, int local, consts def)
 {
-	return HostArraysPtr.m[local] * (HostArraysPtr.S_w[local] * lambda_w (HostArraysPtr, DevArraysPtr, local, def)
-		+ HostArraysPtr.S_n[local] * lambda_n (HostArraysPtr, DevArraysPtr, local, def)
-		+ HostArraysPtr.S_g[local] * lambda_g (HostArraysPtr, DevArraysPtr, local, def)) 
-		+ (1. - HostArraysPtr.m[local]) * lambda_r (HostArraysPtr, DevArraysPtr, local, def);
+	return HostArraysPtr.m[local] * (HostArraysPtr.S_w[local] * lambda_w (HostArraysPtr.T[local], def)
+		+ HostArraysPtr.S_n[local] * lambda_n (HostArraysPtr.T[local], def)
+		+ HostArraysPtr.S_g[local] * lambda_g (HostArraysPtr.T[local], def)) 
+		+ (1. - HostArraysPtr.m[local]) * lambda_r (HostArraysPtr.T[local], def);
 }
 
 // Расчет энтальпии по температуре и теплоемкости
+// !!! Переписать, задав точность для метода Симпсона и передавая указатель на функцию, чтобы не копировать одно и то же
+double assign_H_w (double T, consts def)
+{
+	double integral = 0, sum = 0, h_temp;
+	int N_temp = 50;
+
+	h_temp = (T - T_0) / N_temp;
+	
+
+	integral += с_w(T_0, def);
+	integral += с_w(T, def);
+
+	for(int i = 2; i < N_temp; i+=2)
+		sum += с_w(T_0 + i * h_temp, def);
+
+	sum *= 2;
+	integral += sum;
+	sum = 0;
+
+	for(int i = 1; i < N_temp; i+=2)
+		sum += с_w(T_0 + i * h_temp, def);
+
+	sum *= 4;
+	integral += sum;
+
+	h_temp /= 3;
+	integral *= h_temp;
+
+	return integral;
+}
+
+double assign_H_n (double T, consts def)
+{
+	double integral = 0, sum = 0, h_temp;
+	int N_temp = 50;
+
+	h_temp = (T - T_0) / N_temp;
+
+
+	integral += с_n(T_0, def);
+	integral += с_n(T, def);
+
+	for(int i = 2; i < N_temp; i+=2)
+		sum += с_n(T_0 + i * h_temp, def);
+
+	sum *= 2;
+	integral += sum;
+	sum = 0;
+
+	for(int i = 1; i < N_temp; i+=2)
+		sum += с_n(T_0 + i * h_temp, def);
+
+	sum *= 4;
+	integral += sum;
+
+	h_temp /= 3;
+	integral *= h_temp;
+
+	return integral;
+}
+
+double assign_H_g (double T, consts def)
+{
+	double integral = 0, sum = 0, h_temp;
+	int N_temp = 50;
+
+	h_temp = (T - T_0) / N_temp;
+
+
+	integral += с_g(T_0, def);
+	integral += с_g(T, def);
+
+	for(int i = 2; i < N_temp; i+=2)
+		sum += с_g(T_0 + i * h_temp, def);
+
+	sum *= 2;
+	integral += sum;
+	sum = 0;
+
+	for(int i = 1; i < N_temp; i+=2)
+		sum += с_g(T_0 + i * h_temp, def);
+
+	sum *= 4;
+	integral += sum;
+
+	h_temp /= 3;
+	integral *= h_temp;
+
+	return integral;
+}
+
+void assign_H (ptr_Arrays HostArraysPtr, ptr_Arrays DevArraysPtr, int local, consts def)
+{
+	assign_H_w (HostArraysPtr.T[local], def);
+	assign_H_n (HostArraysPtr.T[local], def);
+	assign_H_g (HostArraysPtr.T[local], def);
+}
 
 // Коэффициенты вязкости для water, napl, gas and rock
 
